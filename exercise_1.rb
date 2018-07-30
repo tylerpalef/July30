50a641c7a80c2254b0743ac8a2fef0949b689cd1
“Tyler <tylerpalef@gmail.com>
2018-07-30 11:06:31 -0400

    finished first bit of exercise 1

---
+++ exercise_1.rb
@@ -0,0 +1,23 @@
+def draw_shape(options)
+  shape = ""
+
+  options[:rows].times do |r|
+    options[:cols].times do |c|
+      shape += options[:char]
+    end
+    shape += "\n"
+  end
+
+  return shape
+
+end
+
+# puts draw_shape(options)
+
+
+  options = {
+    rows: 4,
+    cols: 4,
+    char: "*"
+  }
+  puts draw_shape(options)
